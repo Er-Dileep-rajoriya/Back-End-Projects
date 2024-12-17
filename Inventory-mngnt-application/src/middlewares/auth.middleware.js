347332393d1c21314d92d1fdb64d3f3466160eed
Dileep Rajoriya <139870968+Er-Dileep-rajoriya@users.noreply.github.com>
2024-12-17 22:27:05 +0530

    rr

--- Inventory-mngnt-application/src/middlewares/auth.middleware.js
+++
@@ -1,9 +0,0 @@
-function auth(req, res, next) {
-  if (!req.session.userEmail) {
-    return res.redirect("/login"); // Redirect if the user is not authenticated
-  }
-
-  next(); // Proceed to the next middleware if authenticated
-}
-
-export default auth;
